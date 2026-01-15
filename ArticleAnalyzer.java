7c91a0654ebee1cef4b01afe8d6c72c0ee6be626
camsan26 <155979343+camsan26@users.noreply.github.com>
2026-01-15 19:21:50 +0000

    1/15/26 DN16

--- ArticleAnalyzer.java
+++ ArticleAnalyzer.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ArticleAnalyzer {
     private ArrayList<String> stopWords;
@@ -11,21 +13,31 @@ public class ArticleAnalyzer {
     }
     public static void main(String[] args) {
         ArticleAnalyzer Camila = new ArticleAnalyzer();
+        ArrayList<String> lines = FileOperator.getStringList("data.txt");
+        String line = lines.get(0);
+        Article c = Camila.parseJson(line);
+        System.out.println(c);
+
     }
     public void addArticle(Article article) {
 
     }
     public Article parseJson(String jsonLine) {
-        Article result;
-        Pattern l = Pattern.compile("re");
+        // Pattern l = Pattern.compile("re");
         Pattern h = Pattern.compile("re");
         Pattern c = Pattern.compile("re");
         Pattern e = Pattern.compile("re");
         Pattern a = Pattern.compile("re");
         Pattern d = Pattern.compile("re");
+        
+        Pattern l = Pattern.compile("\"link\":\\s*\"([^\"]+)\"");  //regex to extract words
+        Matcher lm =l.matcher(jsonLine); //parameter - line of text
+        String lt = lm.find() ? lm.group(1) : ""; //extract the destined part
+        Article result=new Article(lt, " "," "," "," "," ");
         return result;
     }
     public String removeStopWords(String text) {
-        String result;
+        String result="";
+        return result;
     }
 }
